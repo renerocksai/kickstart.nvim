af06c835e1cf207c97ea6730d150f3d0d13b1de5
Rene Schallner <rene@renerocks.ai>
2024-10-11 03:50:03 +0200

    trim whitespace on Format / save

--- init.lua
+++ init.lua
@@ -719,11 +719,15 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+
+        -- rene: trim whitespace on all filetypes
+        ['*'] = { 'trim_whitespace' },
       },
     },
   },
