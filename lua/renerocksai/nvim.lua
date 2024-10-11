3e05dd62fbbcb3fe81aa9c8df05b9feb7424aeb5
Rene Schallner <rene@renerocks.ai>
2024-10-11 03:25:52 +0200

    added options (tabs)

--- lua/renerocksai/nvim.lua
+++ lua/renerocksai/nvim.lua
@@ -10,4 +10,8 @@ vim.keymap.set('n', 'g*', 'g*zz', { remap = false })
 vim.keymap.set('n', 'g#', 'g#zz', { remap = false })
 
 -- options
-vim.api.nvim_set_option_value('colorcolumn', '80', {})
+vim.opt.colorcolumn = '80'
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
