8e7851b2300b182113912434579e7154b07a9834
Rene Schallner <rene@renerocks.ai>
2024-10-13 00:06:33 +0200

    superhtml, supermd, ziggy !!! links in queries!!!!!

--- init.lua
+++ init.lua
@@ -655,6 +655,45 @@ require('lazy').setup({
         },
       }
 
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('ziggy', {}),
+        pattern = 'ziggy',
+        callback = function()
+          vim.lsp.start {
+            name = 'Ziggy LSP',
+            cmd = { 'ziggy', 'lsp' },
+            root_dir = vim.loop.cwd(),
+            flags = { exit_timeout = 1000 },
+          }
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('ziggy_schema', {}),
+        pattern = 'ziggy_schema',
+        callback = function()
+          vim.lsp.start {
+            name = 'Ziggy LSP',
+            cmd = { 'ziggy', 'lsp', '--schema' },
+            root_dir = vim.loop.cwd(),
+            flags = { exit_timeout = 1000 },
+          }
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('superhtml', {}),
+        pattern = 'superhtml',
+        callback = function()
+          vim.lsp.start {
+            name = 'SuperHTML LSP',
+            cmd = { 'superhtml', 'lsp' },
+            root_dir = vim.loop.cwd(),
+            flags = { exit_timeout = 1000 },
+          }
+        end,
+      })
+
       -- Ensure the servers and tools above are installed
       --  To check the current status of installed tools and/or manually install
       --  other tools, you can run
@@ -718,8 +757,32 @@ require('lazy').setup({
           lsp_format = lsp_format_opt,
         }
       end,
+      formatters = {
+        superhtml = {
+          inherit = false,
+          command = 'superhtml',
+          stdin = true,
+          args = { 'fmt', '--stdin' },
+        },
+        ziggy = {
+          inherit = false,
+          command = 'ziggy',
+          stdin = true,
+          args = { 'fmt', '--stdin' },
+        },
+        ziggy_schema = {
+          inherit = false,
+          command = 'ziggy',
+          stdin = true,
+          args = { 'fmt', '--stdin-schema' },
+        },
+      },
       formatters_by_ft = {
         lua = { 'stylua' },
+
+        shtml = { 'superhtml' },
+        ziggy = { 'ziggy' },
+        ziggy_schema = { 'ziggy_schema' },
 
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
@@ -998,6 +1061,85 @@ require('lazy').setup({
     },
   },
 })
+
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+
+parser_config.ziggy = {
+  install_info = {
+    url = 'https://github.com/kristoff-it/ziggy',
+    includes = { 'tree-sitter-ziggy/src' },
+    files = { 'tree-sitter-ziggy/src/parser.c' },
+    branch = 'main',
+    generate_requires_npm = false,
+    requires_generate_from_grammar = false,
+  },
+  filetype = 'ziggy',
+}
+
+parser_config.ziggy_schema = {
+  install_info = {
+    url = 'https://github.com/kristoff-it/ziggy',
+    files = { 'tree-sitter-ziggy-schema/src/parser.c' },
+    branch = 'main',
+    generate_requires_npm = false,
+    requires_generate_from_grammar = false,
+  },
+  filetype = 'ziggy-schema',
+}
+
+parser_config.supermd = {
+  install_info = {
+    url = 'https://github.com/kristoff-it/supermd',
+    includes = { 'tree-sitter/supermd/src' },
+    files = {
+      'tree-sitter/supermd/src/parser.c',
+      'tree-sitter/supermd/src/scanner.c',
+    },
+    branch = 'main',
+    generate_requires_npm = false,
+    requires_generate_from_grammar = false,
+  },
+  filetype = 'supermd',
+}
+
+parser_config.supermd_inline = {
+  install_info = {
+    url = 'https://github.com/kristoff-it/supermd',
+    includes = { 'tree-sitter/supermd-inline/src' },
+    files = {
+      'tree-sitter/supermd-inline/src/parser.c',
+      'tree-sitter/supermd-inline/src/scanner.c',
+    },
+    branch = 'main',
+    generate_requires_npm = false,
+    requires_generate_from_grammar = false,
+  },
+  filetype = 'supermd_inline',
+}
+
+parser_config.superhtml = {
+  install_info = {
+    url = 'https://github.com/kristoff-it/superhtml',
+    includes = { 'tree-sitter-superhtml/src' },
+    files = {
+      'tree-sitter-superhtml/src/parser.c',
+      'tree-sitter-superhtml/src/scanner.c',
+    },
+    branch = 'main',
+    generate_requires_npm = false,
+    requires_generate_from_grammar = false,
+  },
+  filetype = 'superhtml',
+}
+
+vim.filetype.add {
+  extension = {
+    smd = 'supermd',
+    shtml = 'superhtml',
+    ziggy = 'ziggy',
+    ['ziggy-schema'] = 'ziggy_schema',
+  },
+}
 
 require 'renerocksai'
 -- The line beneath this is called `modeline`. See `:help modeline`
